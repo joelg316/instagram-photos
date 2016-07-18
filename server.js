28453bcb9e9bc21dede86569d088bf5efa4f0f8c
Joel Ginsberg <joelg316@gmail.com>
2016-07-18 12:10:00 -0500

    Added my project

---
+++ server.js
@@ -0,0 +1,32 @@
+// grab packages
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+// configure app
+app.use(express.static(__dirname + '/public'));
+
+// set view engine to ejs
+app.set('view engine', 'ejs');
+
+ig.use({
+	access_token: '9841118.1677ed0.3c90c1eea6fa48a1b533c83faa11a359'
+});
+
+// set routes
+// home page - profile images
+app.get('/', function(req, res) {
+
+	// use the instagram package to get popular media
+	ig.user_self_media_recent(function(err, medias, pagination, remaining, limit){
+	
+		// render the home page and pass in the popular images
+		res.render('pages/index', { grams: medias });
+	});	
+});
+
+// start the server on port 8080
+app.listen(8080);
+
+// send a message
+console.log('App started on localhost:8080!');
